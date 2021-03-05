c6cd039233417b11739c842e4f2e2a0336f92e7c
kireev.mishka <kireev.mishka@gmail.com>
2021-03-05 12:00:56 +1000

    Update index.js
    all tasks finished

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+exports.min = function min(array) {
+    if (array === undefined || array === []) return 0;
+    else {
+        var min = 0;
+        for (var i = 0; i < array.length; i++) {
+            if (min > array[i]) min = array[i];
+        }
+        return min;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array === []) return 0;
+    else {
+        var max = 0;
+        for (var i = 0; i < array.length; i++)
+            if (max < array[i]) max = array[i];
+        return max;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) return 0;
+    else {
+        const red = (a, b) => a + b;
+        return array.reduce(red, 0) / array.length;
+    }
+};
